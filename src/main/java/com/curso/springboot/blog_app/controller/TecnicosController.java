c595a76abe323b5acf033bc0335d462f8997fbf5
ereyescast <ederreyesc@hotmail.com>
2025-06-17 17:46:03 -0500

    Prueba

--- src/main/java/com/curso/springboot/blog_app/controller/TecnicosController.java
+++ src/main/java/com/curso/springboot/blog_app/controller/TecnicosController.java
@@ -11,5 +11,6 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("api/torneo")
 public class TecnicosController {
-    //Ejemplo
+    //Ejemplo2
+    //Prueba
 }
