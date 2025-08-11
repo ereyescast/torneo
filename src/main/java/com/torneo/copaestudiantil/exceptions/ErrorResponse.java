e3ccd6818cf0e8c771a28bb5d76b9148feb6d856
ereyescast <ederreyesc@hotmail.com>
2025-08-11 01:21:40 -0500

    @Arquitectura Capas3

---
+++ src/main/java/com/torneo/copaestudiantil/exceptions/ErrorResponse.java
@@ -0,0 +1,21 @@
+package com.torneo.copaestudiantil.exceptions;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+import java.time.LocalDateTime;
+
+@Data
+@AllArgsConstructor
+public class ErrorResponse {
+    private String message;
+    private int statusCode;
+    private LocalDateTime timestamp;
+    private String errorDetails;
+
+    public ErrorResponse(String message, int statusCode, String errorDetails) {
+        this.message = message;
+        this.statusCode = statusCode;
+        this.errorDetails = errorDetails;
+    }
+}
--- src/main/java/com/torneo/copaestudiantil/repository/TecnicoRepository.java
+++ src/main/java/com/torneo/copaestudiantil/repository/TecnicoRepository.java
@@ -4,6 +4,13 @@ import com.torneo.copaestudiantil.entity.Tecnico;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
+import java.util.Optional;
+
 @Repository
 public interface TecnicoRepository extends JpaRepository<Tecnico, Long> {
+
+     Optional<Tecnico> findByNombres (String nombres);
+     Optional<Tecnico> findById (Long idTecnico);
+
 }
